255a061328569a4806ffb68c196d2a10643862f7
TexMerph <loshakovpv@yandex.ru>
2021-08-20 19:53:33 +0300

    Lesson 10 commit 1

---
+++ src/main/java/ru/geekbrains/lesson/ten/Main.java
@@ -0,0 +1,42 @@
+package ru.geekbrains.lesson.ten;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> fruits {
+                "Груша",
+                "Лимон",
+                "Апельсин",
+                "Лимон",
+                "Яблоко",
+                "Груша",
+                "Апельсин",
+                "Банан",
+                "Клубника",
+                "Кокос",
+                "Апельсин",
+                "Яблоко",
+                "Арбуз",
+                "Дыня",
+                "Кокос"
+        };
+        checkWords(words);
+
+    }
+    public static void checkWords(String[] words){
+        ArrayList<String> fruits = new ArrayList<String>(Arrays.asList(words));
+        System.out.println(fruits);
+        HashMap<Float, ArrayList<fruits>> fruitbox =
+        int count = 0;
+        String wordBox = "";
+        for (String s: words){
+            if ()
+        }
+        */
+
+    }
+}
